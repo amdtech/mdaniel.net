d6f94d9491f1206a1caeb88b7ff1b1343c533cc3
Aaron Daniel <amdtech@me.com>
2012-02-13 08:13:46 -0800

    Page not found and front page

--- mdaniel.rb
+++ mdaniel.rb
@@ -4,3 +4,16 @@ Bundler.require
 get '/' do
   haml :index
 end
+
+get '/:page' do |page|
+  pass if page == "favicon.ico"
+  begin
+    haml page.to_sym
+  rescue Errno::ENOENT
+    404
+  end
+end
+
+not_found do
+  haml :pnf
+end
